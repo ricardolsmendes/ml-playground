6a8f8319515aa866231850e8136e871841f6f385
Ricardo Lopes de Souza Mendes <ricardolsmendes@gmail.com>
2025-02-14 11:44:26 -0300

    refactor: split the text-to-sql agent classes

---
+++ large-language-models/research-assistants/text-to-sql/agents.py
@@ -0,0 +1,76 @@
+# Most of this code is based on the LangChain's "Build a Question/Answering system over
+# SQL data" tutorial available at https://python.langchain.com/docs/tutorials/sql_qa/.
+
+from typing import Dict
+
+from langchain import hub
+from langchain.chat_models.base import BaseChatModel
+from langchain_community import tools
+from langchain_community.utilities.sql_database import SQLDatabase
+from typing_extensions import Annotated, TypedDict
+
+
+class State(TypedDict):
+    question: str
+    query: str
+    result: str
+    answer: str
+
+
+class QueryOutput(TypedDict):
+    """
+    Generated SQL query.
+    """
+
+    query: Annotated[str, ..., "Syntactically valid SQL query."]
+
+
+class WriteQueryAgent:
+
+    def __init__(self):
+        # See https://smith.langchain.com/hub/langchain-ai/sql-query-system-prompt
+        # for prompt details.
+        self._query_prompt_template = hub.pull("langchain-ai/sql-query-system-prompt")
+
+    def run(self, db: SQLDatabase, state: State, llm: BaseChatModel) -> Dict[str, str]:
+        """
+        Generate SQL query to fetch information.
+        """
+        prompt = self._query_prompt_template.invoke(
+            {
+                "dialect": db.dialect,
+                "top_k": 10,
+                "table_info": db.get_table_info(),
+                "input": state["question"],
+            }
+        )
+        structured_llm = llm.with_structured_output(QueryOutput)
+        return {"query": structured_llm.invoke(prompt)["query"]}
+
+
+class ExecuteQueryAgent:
+
+    @classmethod
+    def run(cls, db: SQLDatabase, state: State) -> Dict[str, str]:
+        """
+        Execute SQL query.
+        """
+        execute_query_tool = tools.QuerySQLDatabaseTool(db=db)
+        return {"result": execute_query_tool.invoke(state["query"])}
+
+
+class GenerateAnswerAgent:
+
+    @classmethod
+    def run(cls, state: State, llm: BaseChatModel) -> Dict[str, str]:
+        """
+        Answer question using retrieved information as context.
+        """
+        prompt = (
+            "Given the following user question, corresponding SQL query, "
+            "and SQL result, answer the user question.\n\n"
+            f'Question: {state["question"]}\n'
+            f'SQL Query: {state["query"]}\n'
+            f'SQL Result: {state["result"]}'
+        )
+        return {"answer": llm.invoke(prompt).content}
---
+++ large-language-models/research-assistants/text-to-sql/assistant.py
@@ -0,0 +1,48 @@
+# Most of this code is based on the LangChain's "Build a Question/Answering system over
+# SQL data" tutorial available at https://python.langchain.com/docs/tutorials/sql_qa/.
+
+from typing import Dict
+
+from langchain import chat_models
+from langchain_community import utilities
+from langgraph import graph
+
+import agents
+from agents import State
+
+
+class Assistant:
+
+    def __init__(self, db_uri: str, model: str, model_provider: str):
+        self._db = utilities.SQLDatabase.from_uri(db_uri)
+        self._llm = chat_models.init_chat_model(model, model_provider=model_provider)
+
+        self._write_query_agent = agents.WriteQueryAgent()
+
+        graph_builder = graph.StateGraph(State).add_sequence(
+            [self._write_query, self._execute_query, self._generate_answer]
+        )
+        graph_builder.add_edge(graph.START, "_write_query")
+        self._compiled_graph = graph_builder.compile()
+
+    def _write_query(self, state: State) -> Dict[str, str]:
+        return self._write_query_agent.run(self._db, state, self._llm)
+
+    def _execute_query(self, state: State) -> Dict[str, str]:
+        return agents.ExecuteQueryAgent.run(self._db, state)
+
+    def _generate_answer(self, state: State) -> Dict[str, str]:
+        return agents.GenerateAnswerAgent.run(state, self._llm)
+
+    def run(self, question: str):
+        """
+        Run the steps in a single sequence.
+        """
+        print(f"\nQUESTION:\n{question}\n")
+
+        for step in self._compiled_graph.stream(
+            {"question": question}, stream_mode="updates"
+        ):
+            for step_key, step_value in step.items():
+                for state, state_value in step_value.items():
+                    print(f"- {state}:\n{state_value}")
